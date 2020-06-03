f7cd4694a5de5c3abaa73d3d272e9aaf6fbe2830
kadepeay <kadepeay@gmail.com>
2020-06-02 22:30:18 -0600

    game plays until quit

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 #!/bin/env python
-import random
+import random, sys
 
 def getNumbers():
     num = random.randint(1,10)
     return num
 
-if __name__ == "__main__":
+def guess():
     num1 = getNumbers()
     num2 = getNumbers()
-    print(f'What is {num1} multiplied by {num2}?')
+    print('"x" to exit\n')
+    print(f'What is {num1} multiplied by {num2}?\n')
     guess = input('Guess: ')
     
     answer = num1 * num2
 
-    if int(guess) == answer:
+    if guess.lower() == 'x':
+        sys.exit()
+    elif int(guess) == answer:
         print('You are correct!')
-        print(f'{num1} multiplied by {num2} is = {answer}')
+        print(f'{num1} multiplied by {num2} is = {answer}\n')
     else:
         print('You are incorrect...')
-        print(f'{num1} multiplied by {num2} is = {answer} not {guess}')
+        print(f'{num1} multiplied by {num2} is = {answer} not {guess}\n') 
+
+
+if __name__ == "__main__":
+    running = True
+    while running:
+        guess()
